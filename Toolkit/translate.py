9868d8cbe45caca497595c351b722a39fb840eb0
Irfannur Diah <kang.fand@gmail.com>
2022-10-11 11:14:07 +0700

    Update translate.py

--- Toolkit/translate.py
+++ Toolkit/translate.py
@@ -1,6 +1,9 @@
 # https://en.wikipedia.org/wiki/IETF_language_tag
+# https://pypi.org/project/translate-api/
 
 # generate translate one word
 # one word -> get all word from i18n
 # sparate common 
 # get simple term
+
+import translators as ts
